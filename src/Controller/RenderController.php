c8573148732db8d7f097cdfed4eb94dde8fd5b00
Vahnra <102360714+Vahnra@users.noreply.github.com>
2022-09-01 10:40:36 +0200

    v0.1

--- src/Controller/RenderController.php
+++ src/Controller/RenderController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Categorie;
+use App\Entity\SousCategorie;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +15,14 @@ class RenderController extends AbstractController
     #[Route('/categories1', name: 'render_categories_in_nav')]
     public function renderCategoriesInNav(EntityManagerInterface $entityManager): Response
     {
-        $categories = $entityManager->getRepository(Categorie::class)->findBy(['deletedAt'=> null]);
+        $categories = $entityManager->getRepository(Categorie::class)->findAll();
+        
+        $souscategories = $entityManager->getRepository(SousCategorie::class)->findAll();
 
         return $this->render('rendered/categories_in_nav.html.twig', [
-            'categories' => $categories
+            'categories' => $categories,
+            'souscategories' => $souscategories
         ]);
     }
+
 }
