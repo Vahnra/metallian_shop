02f7c6e45c6882e46c67666504adae782cd2371f
Vahnra <102360714+Vahnra@users.noreply.github.com>
2022-09-01 23:50:09 +0200

    update rendered navigation

--- src/Controller/RenderController.php
+++ src/Controller/RenderController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Categorie;
 use App\Entity\SousCategorie;
+use App\Entity\CategorieMerchandising;
+use App\Entity\SousCategorieMerchandising;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,14 +16,24 @@ class RenderController extends AbstractController
     #[Route('/categories1', name: 'render_categories_in_nav')]
     public function renderCategoriesInNav(EntityManagerInterface $entityManager): Response
     {
+        // On récupère toutes les categories et sous categories normaux
         $categories = $entityManager->getRepository(Categorie::class)->findAll();
 
         $souscategories = $entityManager->getRepository(SousCategorie::class)->findAll();
 
+        // On récupre toutes les categoriesMerchandising et sousCategoriesMerchandising
+        $categoriesMerchandising = $entityManager->getRepository(CategorieMerchandising::class)->findAll();
+
+        $souscategoriesMerchandising = $entityManager->getRepository(SousCategorieMerchandising::class)->findAll();
+
         return $this->render('rendered/categories_in_nav.html.twig', [
             'categories' => $categories,
-            'souscategories' => $souscategories
+            'souscategories' => $souscategories,
+            'categoriesMerchandising' => $categoriesMerchandising,
+            'souscategoriesMerchandising' => $souscategoriesMerchandising
         ]);
     }
+
+  
 
 }
