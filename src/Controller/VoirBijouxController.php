af94b7f639f8f1538aad2081b121b0c855447644
Liora13 <liorabr@yahoo.com>
2022-09-06 13:38:48 +0200

    voirBijoux

--- src/Controller/VoirBijouxController.php
+++ src/Controller/VoirBijouxController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Bijoux;
+use App\Entity\Color;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,11 +12,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class VoirBijouxController extends AbstractController
 {
     #[Route('/voir/bijoux-{id}', name: 'voir_bijoux', methods: ['GET'])]
-    public function voirBijoux(Bijoux $bijoux ,EntityManagerInterface $entityManager): Response
+    public function voirBijoux(Bijoux $bijoux, Color $colors ,EntityManagerInterface $entityManager): Response
     {
         $bijou = $entityManager->getRepository(Bijoux::class)->findBy(['id'=>$bijoux->getId()]);
+        $color = $entityManager->getRepository(Color::class)->findBy(['id'=>$colors->getId()]);
         return $this->render('voir_bijoux/v_bijoux.html.twig', [
             'bijou' => $bijou,
+            'color' => $color,
             
         ]);
     }
