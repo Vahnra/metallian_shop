5503a5af32debc622ed86bcf34c7f43b4bf550fd
Vahnra <102360714+Vahnra@users.noreply.github.com>
2022-09-07 11:02:08 +0200

    update accessoire

--- src/Controller/VoirAccessoiresController.php
+++ src/Controller/VoirAccessoiresController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Accessoires;
 use App\Entity\Color;
+use App\Entity\Expedition;
 use App\Entity\Material;
 use App\Entity\Size;
+use App\Repository\ExpeditionRepository;
+use App\Repository\VetementRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,13 +16,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class VoirAccessoiresController extends AbstractController
 {
-    #[Route('/voir/accessoires{id}', name: 'voir_accessoires', methods: ['GET'])]
-    public function voirAccessoires(Accessoires $accessoires, Color $colors, Size $sizes, Material $materials, EntityManagerInterface $entityManager): Response
+    #[Route('/voir/accessoires-{id}', name: 'voir_accessoires', methods: ['GET', 'POST'])]
+    public function voirAccessoires(Accessoires $accessoires, EntityManagerInterface $entityManager): Response
     {
         $accessoire = $entityManager->getRepository(Accessoires::class)->findBy(['id'=>$accessoires->getId()]);
-        $color = $entityManager->getRepository(Color::class)->findBy(['id'=>$colors->getId()]);
-        $size = $entityManager->getRepository(Size::class)->findBy(['id'=>$sizes->getId()]);
-        $material = $entityManager->getRepository(Material::class)->findBy(['id'=>$materials->getId()]);
+        
+        $color = $entityManager->getRepository(Color::class)->findBy(['id'=>$accessoire[0]->getColor()]);
+
+        $size = $entityManager->getRepository(Size::class)->findBy(['id'=>$accessoire[0]->getId()]);
+      
+        $material = $entityManager->getRepository(Material::class)->findBy(['id'=>$accessoire[0]->getId()]);
+
+        $expedition = $entityManager->getRepository(Expedition::class)->findAll();
+
         return $this->render('voir_accessoires/voir_accessoires.html.twig', [
             'accessoire' => $accessoire,
             'color' => $color,
