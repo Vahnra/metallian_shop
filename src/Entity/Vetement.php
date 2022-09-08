001d5095a3a798cbec8a9851e1ddfd5517dfe2d5
Liora13 <liorabr@yahoo.com>
2022-09-08 10:23:44 +0200

    gallery vetement

--- src/Entity/Vetement.php
+++ src/Entity/Vetement.php
@@ -67,6 +67,18 @@ class Vetement
     #[ORM\OneToMany(mappedBy: 'vetement', targetEntity: ReviewVetement::class)]
     private Collection $reviewVetements;
 
+    #[ORM\Column(length: 255)]
+    private ?string $photo2 = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $photo3 = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $photo4 = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $photo5 = null;
+
     public function __construct()
     {
         $this->reviewVetements = new ArrayCollection();
@@ -283,6 +295,54 @@ class Vetement
                 $reviewVetement->setVetement(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getPhoto2(): ?string
+    {
+        return $this->photo2;
+    }
+
+    public function setPhoto2(string $photo2): self
+    {
+        $this->photo2 = $photo2;
+
+        return $this;
+    }
+
+    public function getPhoto3(): ?string
+    {
+        return $this->photo3;
+    }
+
+    public function setPhoto3(string $photo3): self
+    {
+        $this->photo3 = $photo3;
+
+        return $this;
+    }
+
+    public function getPhoto4(): ?string
+    {
+        return $this->photo4;
+    }
+
+    public function setPhoto4(string $photo4): self
+    {
+        $this->photo4 = $photo4;
+
+        return $this;
+    }
+
+    public function getPhoto5(): ?string
+    {
+        return $this->photo5;
+    }
+
+    public function setPhoto5(string $photo5): self
+    {
+        $this->photo5 = $photo5;
 
         return $this;
     }
