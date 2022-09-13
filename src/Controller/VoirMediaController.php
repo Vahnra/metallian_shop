cf4d4a3e62c148b75ee6b921afee5b7744aa2608
Liora13 <liorabr@yahoo.com>
2022-09-13 09:42:58 +0200

    media similar Itm

--- src/Controller/VoirMediaController.php
+++ src/Controller/VoirMediaController.php
@@ -26,13 +26,27 @@ class VoirMediaController extends AbstractController
 
         $expedition = $entityManager->getRepository(Expedition::class)->findAll();
 
+        $similarCategory = $entityManager->getRepository(SousCategorie::class)->findBy([
+            'id' => $medium[0]->getSousCategorie()  
+        ]);
 
+        $similarItm = $entityManager->getRepository(Media::class)->findBy([
+            'sousCategorie' => $medium[0]->getSousCategorie(),
+            // 'artist'=>$medium[0]->getArtist()
+        ]);
+        $similaeGnr = $entityManager->getRepository(Media::class)->findBy([
+            'genre' => $medium[0]->getGenre(),
+            // 'artist'=>$medium[0]->getArtist()
+        ]);
+    // dd($medium[0]->getArtist());
 
         return $this->render('voir_media/voir_media.html.twig', [
             'medium' => $medium,
             'artist' => $artist,
             'musicType' => $musicType,
-            'expedition' => $expedition
+            'expedition' => $expedition,
+            'similarItm' => $similarItm,
+            'similaeGnr' => $similaeGnr,
         ]);
     }
 
