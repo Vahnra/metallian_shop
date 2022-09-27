3d300b7184ba5109bd947a7cf857328c858949ff
Vahnra <102360714+Vahnra@users.noreply.github.com>
2022-09-27 09:25:14 +0200

    done size guide

---
+++ src/Controller/SizeGuideController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SizeGuideController extends AbstractController
+{
+    #[Route('/size/guide', name: 'size_guide')]
+    public function index(): Response
+    {
+        return $this->render('size_guide/size_guide.html.twig');
+    }
+}
