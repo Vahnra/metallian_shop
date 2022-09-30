6a5ab04fa3be1696722e61838324db4007891467
Vahnra <102360714+Vahnra@users.noreply.github.com>
2022-09-30 15:18:10 +0200

    upd

--- src/Controller/Admin/MediaQuantityCrudController.php
+++ src/Controller/Admin/MediaQuantityCrudController.php
@@ -3,10 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\MediaQuantity;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
@@ -29,5 +33,21 @@ class MediaQuantityCrudController extends AbstractCrudController
 
         yield FormField::addPanel('Stock');
         yield NumberField::new('stock', 'Nombre en stock');
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Média en vente')
+            ->setEntityLabelInPlural('Médias en vente')
+        ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(EntityFilter::new('media'))
+            ->add(TextFilter::new('sku'))
+            ->add(TextFilter::new('stock'));
     }
 }
