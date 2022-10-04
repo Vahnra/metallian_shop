53c5fe7b6cd43ebd1ad4d95a5fc3b6fe33c12571
Vahnra <102360714+Vahnra@users.noreply.github.com>
2022-10-04 13:15:30 +0200

    entity update

--- src/Controller/RenderController.php
+++ src/Controller/RenderController.php
@@ -117,7 +117,7 @@ class RenderController extends AbstractController
 
         $page = $requestStack->query->getInt('page', 1);
 
-        $searchResults = $paginator->paginate($searchResult, $page, 5);
+        $searchResults = $paginator->paginate($searchResult, $page, 5, array('defaultSortFieldName' => 'a.createdAt', 'defaultSortDirection' => 'desc'));
 
         return $this->render('search_result/search_result.html.twig', [
             'searchResults' => $searchResults,
