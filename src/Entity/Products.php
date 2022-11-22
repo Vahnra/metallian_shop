041d7b95a977e8c7e425e8ee812e9a0ffa813554
Vahnra <102360714+Vahnra@users.noreply.github.com>
2022-11-22 15:16:02 +0100

    update homepage

--- src/Entity/Products.php
+++ src/Entity/Products.php
@@ -29,21 +29,6 @@ class Products
     #[ORM\ManyToOne(inversedBy: 'products')]
     private ?SousCategorie $sousCategorie = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $photo = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $photo2 = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $photo3 = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $photo4 = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $photo5 = null;
-
     #[ORM\ManyToOne(inversedBy: 'products')]
     private ?Marques $marques = null;
 
@@ -158,66 +143,6 @@ class Products
     public function setSousCategorie(?SousCategorie $sousCategorie): self
     {
         $this->sousCategorie = $sousCategorie;
-
-        return $this;
-    }
-
-    public function getPhoto(): ?string
-    {
-        return $this->photo;
-    }
-
-    public function setPhoto(?string $photo): self
-    {
-        $this->photo = $photo;
-
-        return $this;
-    }
-
-    public function getPhoto2(): ?string
-    {
-        return $this->photo2;
-    }
-
-    public function setPhoto2(?string $photo2): self
-    {
-        $this->photo2 = $photo2;
-
-        return $this;
-    }
-
-    public function getPhoto3(): ?string
-    {
-        return $this->photo3;
-    }
-
-    public function setPhoto3(?string $photo3): self
-    {
-        $this->photo3 = $photo3;
-
-        return $this;
-    }
-
-    public function getPhoto4(): ?string
-    {
-        return $this->photo4;
-    }
-
-    public function setPhoto4(?string $photo4): self
-    {
-        $this->photo4 = $photo4;
-
-        return $this;
-    }
-
-    public function getPhoto5(): ?string
-    {
-        return $this->photo5;
-    }
-
-    public function setPhoto5(?string $photo5): self
-    {
-        $this->photo5 = $photo5;
 
         return $this;
     }
